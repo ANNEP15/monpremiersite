d28654dd2222ebd6189b3dc9180cc9e4b5ef4676
Annette Plaisimont <plaisimont320@hotmail.com>
2020-11-09 20:47:50 +0100

    pl

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
+<div> id="app>"
   <img alt="Vue logo" src="./assets/logo.png" />
+</div> 
   <Bienvenu sur le site d'Annette />
 </template>
-
+<script>
 <  HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-    
+
   }
 }
 script>
