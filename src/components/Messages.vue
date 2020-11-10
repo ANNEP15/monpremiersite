508ebd89d6cb480cdfd206300725ada8668de293
Annette Plaisimont <plaisimont320@hotmail.com>
2020-11-10 18:49:52 +0100

    import bootstrap avec navbar moche

---
+++ src/components/Messages.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Messages",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
