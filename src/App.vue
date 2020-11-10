79bc2e0fc88a18dd2c2a5a40ae1c50bfeb545426
Annette Plaisimont <plaisimont320@hotmail.com>
2020-11-10 18:21:55 +0100

    version avec uniquement message d'Annette

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
+
 <template>
-<div> id="app>"
- <img alt="Vue logo" src=".assets/logo.png"/>
- <Bienvenu sur le site Annette/>
- 
-<messages/>
-  </div>
-  
+  <Messages msg="Bienvenu sur le site d'Annette" />
 </template>
 
 <script>
-<  messages from './components/messages.vue'>
-
+import Messages from './components/Messages.vue'
 export default {
   name: 'App',
   components: {
-    messages
+    Messages
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/messages.vue
+++
@@ -1,6 +0,0 @@
-<template> 
-<div>
-    <h1>Bienvenu sur le site d'Annette</h1>
-    </div>
-
-</template>
