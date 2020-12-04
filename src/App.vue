2ca35c5b1799f90fbe6fc02fc2f25d3246ff9318
Annette Plaisimont <plaisimont320@hotmail.com>
2020-12-04 17:43:48 +0100

    commit home propre

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,61 @@
         <li class="nav-item active">
           <a class="nav-link" href="#">Home <span class="sr-only">Home</span></a>
         </li>
+        <li>
+          <a class="nav-link" href="mailto:annette.plaisimont@sgcib.com" title="Contact">Contact</a>
+        </li>
+       
         <li class="nav-item active">
           <a class="nav-link" href="#tableau">Tableau <span class="sr-only">Tableau</span></a>
         </li>
-        <img src="./assets/logo.jpg"/>
+        <li class="float-right">
+        <img class="float-right" src="./assets/logo.jpg"/>
+        </li>
+        
+                     
+            
+                       
     </div>
+    
   </nav>
+                
   <Messages msg="Hello - Bienvenu sur le site d'Annette" />
-  <!-- Footer -->
- <div id="footer">Je suis le footer </div> 
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./assets/femme flamme2.jpg" alt="Tableau 1">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./assets/MER2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+             <!-- Footer -->
+ <div id="footer">e-mail </div> 
+  <div class="col-sm-4 offset-md-1 py-4">
+          
+          <ul class="list-unstyled">
+              <li><a href="#" class="text-white">Contact</a></li>
+          </ul>
+        </div>
 <!-- Footer -->
 </template>
 
