8fa2cf4e8f0a026def53853663c338203c07e67c
Annette Plaisimont <plaisimont320@hotmail.com>
2020-12-08 18:04:16 +0100

    navbar image à droite

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,22 @@
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+         <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+           </li>
+           <li class="nav-item">
+           <a class="nav-link" href="#tableau">Tableau</a>
+          </li>
+        </ul>
+       <img src="./assets/logo.jpg"/>
+ </div>
+ </nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">	     <img src="./assets/logo.jpg"/>
+      <ul class="navbar-nav mr-auto">	  </div>
+        <li class="nav-item active">	</nav>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">Home</span></a>
-        </li>
-        <li>
-          <a class="nav-link" href="mailto:annette.plaisimont@sgcib.com" title="Contact">Contact</a>
-        </li>
-       
-        <li class="nav-item active">
-          <a class="nav-link" href="#tableau">Tableau <span class="sr-only">Tableau</span></a>
-        </li>
-        <li class="float-right">
-        <img class="float-right" src="./assets/logo.jpg"/>
-        </li>
-                    
-                            
-    </div>
-    
-  </nav>
-                
   <Messages msg="Hello - Bienvenu sur le site d'Annette" />
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
